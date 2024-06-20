0e2452489a4c584783a21caae2232fc85ec04728
Asghar Ali <asghar@dotandlinelearning.com>
2024-06-20 16:16:06 +0500

    work with mysql

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'nodeudemy'
-})
+    host: 'localhost',
+    user: 'root',
+    database: 'nodeudemy',
+    password: ''
+});
 
 module.exports = pool.promise();
