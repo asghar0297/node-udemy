07aa93299b7c960decfdd96370e25345e2a1fe2a
Asghar Ali <asghar@dotandlinelearning.com>
2024-06-21 18:47:43 +0500

    add sequlize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,17 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodeudemy',
-    password: ''
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('nodeudemy', 'root', '',{
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'nodeudemy',
+//     password: ''
+// });
+
+// module.exports = pool.promise();
