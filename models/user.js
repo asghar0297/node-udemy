cc0af473316038d6b89f03e428588fdbaed3a48d
Asghar Ali <asghar.ali@waada.pk>
2026-02-28 03:29:57 +0500

    show orders on order page

--- models/user.js
+++ models/user.js
@@ -109,6 +109,12 @@ class User {
     })
   }
 
+  getOrders(){
+      const db = getDb();
+      return db.collection('orders').find({ 'user._id' : new ObjectId(this._id) })
+      .toArray()
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
